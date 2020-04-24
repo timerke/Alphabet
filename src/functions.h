a679a2ca9c9e7cd32e12659de4032d1c6be01cf9
timerke <timerke@mail.ru>
2020-04-24 23:39:22 +0300

    Написана функция для определения способа передачи текста в программу.

---
+++ src/functions.h
@@ -0,0 +1,16 @@
+﻿/*
+Заголовочный файл functions.h содержит объявления функций,
+используемых в программе "Алфавит".
+*/
+
+#pragma once
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+#include <vector>
+
+char determine_way(int, char* []);
+void finish();
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+﻿/*
+Приложение "Алфавит" читает текст и выводит из него по одному
+слову в строке по алфавиту.
+*/
+
+#include <clocale>
+#include <iostream>
+#include "functions.h"
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Rus");
+
+	// Определяем способ передачи входного текста
+	char response = determine_way(argc, argv);
+
+	// Выход из приложения
+	finish();
+	return 0;
+}
